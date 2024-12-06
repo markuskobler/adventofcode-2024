515759caa8f00627b37f8141a2dd2d57eb8d9308
Markus Kobler <markus@distinctive.co>
2024-12-05 20:57:01 -0500

    Day 4 part 2

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,11 +1,14 @@
 fn main() {
     let input = include_str!("../../data/day04.txt");
 
-    let part1 = process_board(input);
+    let part1 = process_part1(input);
     eprintln!("Part 1: {part1}");
+
+    let part2 = process_part2(input);
+    eprintln!("Part 2: {part2}");
 }
 
-fn process_board(board: &str) -> usize {
+fn process_part1(board: &str) -> usize {
     let width = board.find("\n").unwrap();
     let mut count = 0;
 
@@ -46,7 +49,6 @@ fn find_xmas(board: &str, width: usize, offset: usize, x: isize, y: isize) -> bo
     let mut o = offset as isize;
 
     let max_x = (o % (width as isize + 1)) + (x * 3);
-
     if max_x < 0 || max_x > width as isize {
         return false;
     }
@@ -73,6 +75,56 @@ fn find_xmas(board: &str, width: usize, offset: usize, x: isize, y: isize) -> bo
     return true;
 }
 
+fn process_part2(board: &str) -> usize {
+    let width = board.find("\n").unwrap();
+    let mut count = 0;
+
+    for (idx, ch) in board.chars().into_iter().enumerate() {
+        if 'A' == ch {
+            if find_mas(board, width, idx) {
+                count += 1;
+            }
+        }
+    }
+
+    count
+}
+
+fn find_mas(board: &str, width: usize, offset: usize) -> bool {
+    let board = board.as_bytes();
+
+    let x = offset % (width + 1);
+    let y = offset / (width + 1);
+
+    if x < 1 || x >= width - 1 || y < 1 || y >= width - 1 {
+        return false;
+    }
+
+    let mut count = 0;
+    if b'M' == board[offset as usize - 1 - (width + 1)]
+        && b'S' == board[offset as usize + 1 + (width + 1)]
+    {
+        count += 1;
+    }
+    if b'M' == board[offset as usize + 1 - (width + 1)]
+        && b'S' == board[offset as usize - 1 + (width + 1)]
+    {
+        count += 1;
+    }
+    if b'M' == board[offset as usize + 1 + (width + 1)]
+        && b'S' == board[offset as usize - 1 - (width + 1)]
+    {
+        count += 1;
+    }
+    if b'M' == board[offset as usize - 1 + (width + 1)]
+        && b'S' == board[offset as usize + 1 - (width + 1)]
+    {
+        count += 1;
+    }
+
+    return count == 2;
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -91,6 +143,7 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
 
-        assert_eq!(process_board(board), 18);
+        assert_eq!(process_part1(board), 18);
+        assert_eq!(process_part2(board), 9);
     }
 }
